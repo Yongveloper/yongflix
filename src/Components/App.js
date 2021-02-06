41379513075f0929b5796a927e5a0936a77cf98b
Yongveloper <aaa9015@naver.com>
2021-02-06 17:28:17 +0900

    GlobalStyles and Header

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Router from 'Components/Router';
+import GloablSyles from 'Components/GlobalStyle';
 
 class App extends Component {
   render() {
     return (
       <>
         <Router />
+        <GloablSyles />
       </>
     );
   }
