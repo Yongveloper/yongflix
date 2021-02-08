612591dadad7bc049ac40c5114aee590bf1d8fae
Yongveloper <aaa9015@naver.com>
2021-02-08 23:01:52 +0900

    Search Container

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import SearchPresenter from './SearchPresenter';
+import { moviesApi, tvApi } from 'api';
 
-export default class extends React.Component {
+class SearchContainer extends React.Component {
   state = {
     movieResults: null,
     tvResults: null,
     searchTerm: '',
     loading: false,
     error: null,
+  };
+
+  handleSubmit = () => {
+    const { searchTerm } = this.state;
+    if (searchTerm !== '') {
+      this.searchByterm();
+    }
+  };
+
+  searchByterm = async () => {
+    const { searchTerm } = this.state;
+    this.setState({ loading: true });
+    try {
+      const {
+        data: { results: movieResults },
+      } = await moviesApi.search(searchTerm);
+      const {
+        data: { results: tvResults },
+      } = await tvApi.search(searchTerm);
+      this.setState({
+        movieResults,
+        tvResults,
+      });
+    } catch {
+      this.setState({ error: '검색 결과가 없습니다.' });
+    } finally {
+      this.setState({ loading: false });
+    }
   };
 
   render() {
@@ -19,7 +48,10 @@ export default class extends React.Component {
         searchTerm={searchTerm}
         loading={loading}
         error={error}
+        handleSubmit={this.handleSubmit}
       />
     );
   }
 }
+
+export default SearchContainer;
