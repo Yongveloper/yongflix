f0c3b7e8a2dd3671499637e2105bc9f2b1fd7860
Yongveloper <aaa9015@naver.com>
2021-02-10 23:10:22 +0900

    Detail Container part Two

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,12 +1,20 @@
+import { moviesApi, tvApi } from 'api';
 import React from 'react';
 import DetailPresenter from './DetailPresenter';
 
 class DetailContainer extends React.Component {
-  state = {
-    result: null,
-    error: null,
-    loading: true,
-  };
+  constructor(props) {
+    super(props);
+    const {
+      location: { pathname },
+    } = props;
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+      isMovie: pathname.includes('/movie/'),
+    };
+  }
 
   async componentDidMount() {
     const {
@@ -15,14 +23,31 @@ class DetailContainer extends React.Component {
       },
       history: { push },
     } = this.props;
+    const { isMovie } = this.state;
     const parsedId = parseInt(id);
     if (isNaN(parsedId)) {
       return push('/');
+    }
+    let result = null;
+    try {
+      if (isMovie) {
+        const request = await moviesApi.moiveDetail(parsedId);
+        result = request.data;
+      } else {
+        const request = await tvApi.showDetail(parsedId);
+        result = request.data;
+      }
+      console.log(result);
+    } catch {
+      this.setState({ error: '검색결과를 찾지 못했습니다.' });
+    } finally {
+      this.setState({ loading: false, result });
     }
   }
 
   render() {
     const { result, error, loading } = this.state;
+    console.log(this.state);
     return <DetailPresenter result={result} error={error} loading={loading} />;
   }
 }
