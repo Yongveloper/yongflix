48b01d8d438cac9b273b01df7b02e49bed2d9934
Yongveloper <aaa9015@naver.com>
2021-02-24 19:51:50 +0900

    HomeContainer.js: class-> function 컴포넌트로 변경

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,17 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import HomePresenter from './HomePresenter';
 import { moviesApi } from 'api';
 
-class HomeContainer extends React.Component {
-  state = {
-    nowPlaying: null,
-    upcoming: null,
-    popular: null,
-    error: null,
-    loading: true,
-  };
+const HomeContainer = () => {
+  const [nowPlaying, setNowPlaying] = useState(null);
+  const [upcoming, setUpcoming] = useState(null);
+  const [popular, setPopular] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  async componentDidMount() {
+  useEffect(async () => {
     try {
       const {
         data: { results: nowPlaying },
@@ -22,34 +20,75 @@ class HomeContainer extends React.Component {
       const {
         data: { results: popular },
       } = await moviesApi.popular();
-      this.setState({
-        nowPlaying,
-        upcoming,
-        popular,
-      });
+      setNowPlaying(nowPlaying);
+      setUpcoming(upcoming);
+      setPopular(popular);
     } catch {
-      this.setState({
-        error: '영화의 정보를 찾을 수 없습니다.',
-      });
+      setError('영화의 정보를 찾을 수 없습니다.');
     } finally {
-      this.setState({
-        loading: false,
-      });
+      setLoading(false);
     }
-  }
+  }, []);
 
-  render() {
-    const { nowPlaying, upcoming, popular, error, loading } = this.state;
-    return (
-      <HomePresenter
-        nowPlaying={nowPlaying}
-        upcoming={upcoming}
-        popular={popular}
-        error={error}
-        loading={loading}
-      />
-    );
-  }
-}
+  return (
+    <HomePresenter
+      nowPlaying={nowPlaying}
+      upcoming={upcoming}
+      popular={popular}
+      error={error}
+      loading={loading}
+    />
+  );
+};
+
+// class HomeContainer extends React.Component {
+//   state = {
+//     nowPlaying: null,
+//     upcoming: null,
+//     popular: null,
+//     error: null,
+//     loading: true,
+//   };
+
+//   async componentDidMount() {
+//     try {
+//       const {
+//         data: { results: nowPlaying },
+//       } = await moviesApi.nowPlaying();
+//       const {
+//         data: { results: upcoming },
+//       } = await moviesApi.upcoming();
+//       const {
+//         data: { results: popular },
+//       } = await moviesApi.popular();
+//       this.setState({
+//         nowPlaying,
+//         upcoming,
+//         popular,
+//       });
+//     } catch {
+//       this.setState({
+//         error: '영화의 정보를 찾을 수 없습니다.',
+//       });
+//     } finally {
+//       this.setState({
+//         loading: false,
+//       });
+//     }
+//   }
+
+//   render() {
+//     const { nowPlaying, upcoming, popular, error, loading } = this.state;
+//     return (
+//       <HomePresenter
+//         nowPlaying={nowPlaying}
+//         upcoming={upcoming}
+//         popular={popular}
+//         error={error}
+//         loading={loading}
+//       />
+//     );
+//   }
+// }
 
 export default HomeContainer;
