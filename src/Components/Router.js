5db17d95522a63ed032113d9034ed21dd4e18d4c
Yongveloper <aaa9015@naver.com>
2021-03-21 22:58:12 +0900

    Router.js: Delete exact in /tv

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -17,7 +17,7 @@ const Routers = () => (
       <Header />
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/tv" exact component={TV} />
+        <Route path="/tv" component={TV} />
         <Route path="/search" component={Search} />
         <Route path="/movie/:id" component={Detail} />
         <Route path="/show/:id" component={Detail} />
