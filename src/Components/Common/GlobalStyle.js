00f3f9621a42e0390b7c1d0d981ea3ad5355f9f4
Yongveloper <aaa9015@naver.com>
2021-03-22 23:48:23 +0900

    Apply google font

--- src/Components/Common/GlobalStyle.js
+++ src/Components/Common/GlobalStyle.js
@@ -1,31 +1,23 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
-import GmarketSansTTFMedium from '../../font/GmarketSansTTFMedium.ttf';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const globalStyles = createGlobalStyle`
     ${reset};
-    @font-face {
-    font-family: 'Gmarket Sans';
-    src: url(${GmarketSansTTFMedium}) format('woff2');
-    font-weight: 300;
-    font-style: normal;
-    font-display: auto;
-  }
     * {
-        box-sizing:border-box;     
+        box-sizing:border-box;
     }
     body{
-        font-family: 'Gmarket Sans';
+        font-family: 'Noto Sans KR', sans-serif;
         font-size: 16px;
         background-color:#1b1b1b;
         color: white;       
         padding-top:50px;
-    }
     a{
         text-decoration: none;
         color:inherit;
+    }
     }
 `;
 
