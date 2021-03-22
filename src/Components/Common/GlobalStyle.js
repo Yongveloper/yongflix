afccd706f0f9a44b3bf6ec2c793c8c037d84bacd
Yongveloper <aaa9015@naver.com>
2021-03-22 20:20:36 +0900

    Change body  font

--- src/Components/Common/GlobalStyle.js
+++ src/Components/Common/GlobalStyle.js
@@ -1,23 +1,31 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
+import GmarketSansTTFMedium from '../../font/GmarketSansTTFMedium.ttf';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const globalStyles = createGlobalStyle`
     ${reset};
-    a{
-        text-decoration: none;
-        color:inherit;
-    }
+    @font-face {
+    font-family: 'Gmarket Sans';
+    src: url(${GmarketSansTTFMedium}) format('truetype');
+    font-weight: 300;
+    font-style: normal;
+    font-display: auto;
+  }
     * {
-        box-sizing:border-box;
+        box-sizing:border-box;     
     }
     body{
-        font-family:--apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-family: 'Gmarket Sans';
         font-size: 16px;
         background-color:#1b1b1b;
         color: white;       
         padding-top:50px;
+    }
+    a{
+        text-decoration: none;
+        color:inherit;
     }
 `;
 
