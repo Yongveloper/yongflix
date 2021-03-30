09ff1221949c6ba4b35bcc4a6e08e7f4eb4e8e9e
YongCoding <64254228+Yongveloper@users.noreply.github.com>
2021-03-30 21:22:40 +0900

    API KEY 숨김처리 오류로 인한 되돌림

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
   params: {
-    api_key: process.env.REACT_APP_API_KEY,
+    api_key: '17603f87d6ac9f83a2905f12c9f181b9',
     language: 'ko-KR',
     region: 'KR',
   },
