4df2abe1ce7da9776bcbcaca07548b6ae826a358
Yongveloper <aaa9015@naver.com>
2021-03-31 20:15:50 +0900

    API KEY 오류로 인한 API KEY 문자열로 처리

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiKey = process.env.REACT_APP_API_KEY;
+const apiKey = `${process.env.REACT_APP_API_KEY}`;
 
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
