b5bced18d5b3c5512ae4a84f4c3b9288b207b808
Yongveloper <aaa9015@naver.com>
2021-03-31 20:18:28 +0900

    API KEY 변수로 사용 안하게 함

--- src/api.js
+++ src/api.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
-const apiKey = `${process.env.REACT_APP_API_KEY}`;
-
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
   params: {
-    api_key: apiKey,
+    api_key: process.env.REACT_APP_API_KEY,
     language: 'ko-KR',
     region: 'KR',
   },
